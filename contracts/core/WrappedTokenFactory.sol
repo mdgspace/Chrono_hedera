eea9ba9d245d76efec436ace3154d48283cc3588
LordRyuga <saketishaan123@gmail.com>
2026-08-09 02:34:36 +0530

    feat: adds wrapped token factory

---
+++ contracts/core/WrappedTokenFactory.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {HederaTokenService} from "@hiero-ledger/hiero-contracts/token-service/HederaTokenService.sol";
+import {KeyHelper} from "@hiero-ledger/hiero-contracts/token-service/KeyHelper.sol";
+import {ExpiryHelper} from "@hiero-ledger/hiero-contracts/token-service/ExpiryHelper.sol";
+import {HederaResponseCodes} from "@hiero-ledger/hiero-contracts/common/HederaResponseCodes.sol";
+import {IHederaTokenService} from "@hiero-ledger/hiero-contracts/token-service/IHederaTokenService.sol";
+
+contract WrappedTokenFactory is HederaTokenService, KeyHelper, ExpiryHelper {
+    mapping(string => address) public tokens;
+    
+    event TokenCreated(string symbol, address tokenAddress);
+
+    function createWrappedToken(
+        string memory name,
+        string memory symbol,
+        uint32 decimals,
+        uint256 initialSupply
+    ) external payable returns (address) {
+        IHederaTokenService.HederaToken memory token;
+        token.name = name;
+        token.symbol = symbol;
+        token.treasury = address(this);
+        token.expiry = createAutoRenewExpiry(address(this), 7776000); // 90 days
+
+        IHederaTokenService.TokenKey[] memory keys = new IHederaTokenService.TokenKey[](1);
+        keys[0] = getSingleKey(KeyType.SUPPLY, KeyValueType.CONTRACT_ID, address(this));
+        token.tokenKeys = keys;
+
+        (int responseCode, address created) = createFungibleToken(token, int64(int256(initialSupply)), int32(uint32(decimals)));
+        require(responseCode == HederaResponseCodes.SUCCESS, "Token creation failed");
+
+        tokens[symbol] = created;
+        emit TokenCreated(symbol, created);
+        
+        return created;
+    }
+}
